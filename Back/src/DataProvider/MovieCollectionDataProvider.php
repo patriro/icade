9167442c0d2c8420fb3b2b3a82eb72bb42f68e7a
patriro <olaripatrice@gmail.com>
2021-01-15 13:08:55 +0100

    check if there is any genre filter

--- Back/src/DataProvider/MovieCollectionDataProvider.php
+++ Back/src/DataProvider/MovieCollectionDataProvider.php
@@ -3,6 +3,8 @@ namespace App\DataProvider;
 
 use App\Entity\Movie;
 use App\Service\TMDBService;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGenerator;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use ApiPlatform\Core\Exception\ResourceClassNotSupportedException;
 use ApiPlatform\Core\DataProvider\ContextAwareCollectionDataProviderInterface;
@@ -11,8 +13,11 @@ final class MovieCollectionDataProvider implements ContextAwareCollectionDataPro
 {
     private $tmdbService;
 
-    public function __construct(TMDBService $tmdbService)
+    private $itemExtensions;
+
+    public function __construct(iterable $itemExtensions, TMDBService $tmdbService)
     {
+        $this->itemExtensions = $itemExtensions;
         $this->tmdbService = $tmdbService;
     }
 
@@ -23,7 +28,13 @@ final class MovieCollectionDataProvider implements ContextAwareCollectionDataPro
 
     public function getCollection(string $resourceClass, string $operationName = null, array $context = []): iterable
     {
-        $results = $this->tmdbService->findAllMovies();
+        $genreId = null;
+
+        if (!empty($context["filters"]) && array_key_exists("genre", $context["filters"])) {
+            $genreId = $context["filters"]["genre"];
+        }
+
+        $results = $this->tmdbService->findAllMovies($genreId);
 
         $page = $results["page"];
 
