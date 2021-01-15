d925cc2ad035e3947bb19661c5a82ec7abd5d41b
patriro <olaripatrice@gmail.com>
2021-01-15 14:18:12 +0100

    rename to more understanding

--- Back/src/Filter/CustomFilter.php
+++ Back/src/Filter/CustomFilter.php
@@ -8,7 +8,7 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\AbstractContextAwareFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\ContextAwareFilterInterface;
 
-final class GenreFilter extends AbstractContextAwareFilter
+final class CustomFilter extends AbstractContextAwareFilter
 {
     protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null) { }
 
